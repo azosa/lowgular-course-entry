6d282d22f1dcdf9cee19e94991c0fb4023c077c0
Paulina <azosa93@gmail,com>
2022-11-18 21:00:24 +0100

    event binding added

--- src/app/ui/employee-form/employee-form.component.ts
+++ src/app/ui/employee-form/employee-form.component.ts
@@ -11,7 +11,10 @@ export class EmployeeFormComponent {
   readonly employeeForm: FormGroup = new FormGroup(
     {
       name: new FormControl(null,[Validators.required]),
-  age: new FormControl(null, [Validators.min(0)]) ,
+  age: new FormControl(null, [Validators.min(0)]),
   salary: new FormControl(null, [Validators.min(0),Validators.required])
 })
+  onButtonClicked(form:{name:string,age:number,salary:number}){
+    alert('User was successfully added to the database. Name: '+form.name+', Age: '+form.age+', Salary: '+form.salary);
+  }
 }
