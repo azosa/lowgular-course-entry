349c6f88cfd4ba5b29b52a6689d53132723ebfee
Paulina <azosa93@gmail,com>
2022-11-18 23:31:41 +0100

    remove employee added

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Observable} from "rxjs";
-import {PersonModel} from "../model/person.model";
-import {CreateEmployeeModel} from "../model/create-employee.model";
-import {map} from "rxjs/operators";
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { PersonModel } from '../model/person.model';
+import { CreateEmployeeModel } from '../model/create-employee.model';
 
 @Injectable()
 export class EmployeeService {
   constructor(private _httpClient: HttpClient) {
   }
-  getAll():Observable<PersonModel[]>{
+  getAll(): Observable<PersonModel[]> {
     return this._httpClient.get<PersonModel[]>('assets/data/people.json')
   }
 
   create(employee: CreateEmployeeModel): Observable<any> {
     return this._httpClient.post('https://dummy.restapiexample.com/api/v1/create', employee).pipe(map(data => void 0));
+  }
+  delete(id: string): Observable<void> {
+    return this._httpClient.delete('https://dummy.restapiexample.com/api/v1/delete/'+id).pipe(map(_=>void 0))
+
   }
 }
--- src/app/ui/employee-list/employee-list.component.ts
+++ src/app/ui/employee-list/employee-list.component.ts
@@ -1,7 +1,8 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { EmployeeService } from '../../services/employee.service';
-import {PersonModel} from "../../model/person.model";
+import { PersonModel } from '../../model/person.model';
 
 @Component({
   selector: 'employee-list',
@@ -11,7 +12,12 @@ import {PersonModel} from "../../model/person.model";
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class EmployeeListComponent {
-  constructor(private _employeeService: EmployeeService) { }
-  data$: Observable<PersonModel[] | null> =this._employeeService.getAll();
+  constructor(private _employeeService: EmployeeService, private _httpClient: HttpClient) { }
+  data$: Observable<PersonModel[] | null> = this._employeeService.getAll();
   text$: string = 'my additional text';
+  remove(id: string) {
+    this._employeeService.delete(id).subscribe();
+
+
+  }
 }
