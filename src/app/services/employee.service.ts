ee64b1a0aa1b8617655094126de62bb6d234ec7c
Paulina <azosa93@gmail,com>
2022-11-19 00:38:22 +0100

    response added

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -4,13 +4,28 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { PersonModel } from '../model/person.model';
 import { CreateEmployeeModel } from '../model/create-employee.model';
-
+import {ApiResponse} from "./api.response";
+import {EmployeeResponse} from "./employee.response";
 @Injectable()
 export class EmployeeService {
   constructor(private _httpClient: HttpClient) {
   }
   getAll(): Observable<PersonModel[]> {
-    return this._httpClient.get<PersonModel[]>('assets/data/people.json')
+    return this._httpClient.get<ApiResponse<EmployeeResponse[]>>(
+      'https://dummy.restapiexample.com/api/v1/employees',
+      ).pipe(
+        map((response:ApiResponse<EmployeeResponse[]>)=>{
+          return response.data.map((employeeResponse:EmployeeResponse)=> {
+            return {
+              name: employeeResponse.employee_name,
+              personalNumber: employeeResponse.id,
+              img: employeeResponse.profile_image,
+              mail: employeeResponse.employee_name + '@lowgular.io'
+
+            }
+          });
+        })
+    )
   }
 
   create(employee: CreateEmployeeModel): Observable<any> {
