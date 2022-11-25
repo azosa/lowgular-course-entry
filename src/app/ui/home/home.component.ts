49acae089adeafa695c459396d2d0dddfffee61a
Paulina <azosa93@gmail,com>
2022-11-25 22:33:52 +0100

    feat: home page added

---
+++ src/app/ui/home/home.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomeComponent {
+  b$= '/create-employee';
+  a$='/employee-list';
+}
